30ba29dbcc9027eb6dd4efdc1e7be881e4297a32
ioni5 <ioni5.goto@gmail.com>
2023-04-11 18:52:23 -0400

    refactor #43

---
+++ src/views/Console.java
@@ -0,0 +1,65 @@
+package views;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Console {
+    
+    private static final Scanner sc = new Scanner(System.in);
+
+    public String read() {
+        String input = null;
+        try {
+            input = sc.next();
+        } catch(Exception ex) {
+            this.write("Error: Invalid input");
+            System.exit(0);
+        }
+        return input;
+    }
+
+    public int readInt() {
+        int input = 0;
+        try {
+            input = Integer.parseInt(this.read());
+        } catch (Exception ex) {
+            this.write("Error: Invalid input");
+            System.exit(0);
+        }
+        return input;
+    }
+
+    public void write(String message) {
+        System.out.println(message);
+    }
+
+    public String readEmail() {
+        String regex = "^(.)+@(.)+\\.com$";
+        Pattern pattern = Pattern.compile(regex);
+        boolean error = false;
+        String email;
+        do {
+            System.out.println("Enter Email format: name@domain.com");
+            email = sc.next();
+            error = !pattern.matcher(email).matches();
+            if (error) {
+                System.out.println("Error: Invalid Email format");
+            }
+        } while (error);
+        return email;
+    }
+
+    public boolean readYesOrNot() {
+        String yesOrNot;
+        boolean error = false;
+        System.out.println("Would you like to add another room y/n");
+        do {
+            yesOrNot = sc.next();
+            error = !yesOrNot.equalsIgnoreCase("y") && !yesOrNot.equalsIgnoreCase("n");
+            if (error) {
+                System.out.println("Please enter Y (Yes) or N (No)");
+            }
+        } while (error);
+        return yesOrNot.equalsIgnoreCase("y");
+    }
+}
---
+++ src/views/MainMenu.java
@@ -0,0 +1,27 @@
+package views;
+
+public class MainMenu {
+
+    private static final Console console = new Console();
+
+    private static final String[] OPTIONS = {
+        "1. Create an account",
+        "2. Admin",
+        "3. Exit"
+    };
+
+    public void show() {
+        console.write("Welcome to the Hotel Reservation Application");
+        console.write("-------------------------------------------");
+        for (String option : OPTIONS) {
+            console.write(option) ;
+        }
+        console.write("-------------------------------------------");
+        console.write("Please select a number for the menu option");
+    }
+
+    public int getOption() {
+        return console.readInt();
+    }
+    
+}
