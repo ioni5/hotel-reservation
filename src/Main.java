126f4b50cf3d9fa7a8fbe81bde5a604d9fae6a9b
ioni5 <115875839+ioni5@users.noreply.github.com>
2023-04-11 03:56:39 +0000

    add option #25

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
+    private static final Scanner sc = new Scanner(System.in);
+
+    public void start() {
+        int option = 0;
+        do {
+            System.out.println("Admin Menu");
+            System.out.println("------------------------------");
+            System.out.println("1. See all Customers");
+            System.out.println("------------------------------");
+            System.out.println("Please select a number for the menu option");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Option 1 selected.");
+                    break;
+                default:
+                    break;
+            }
+        } while (true);
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hotel Reservation");
+        new Main().start();
     }
 }
