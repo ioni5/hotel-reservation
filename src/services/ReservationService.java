7543c6365a1946a55d71b59c3ca40a4935b13631
ioni5 <115875839+ioni5@users.noreply.github.com>
2023-04-12 17:36:08 +0000

    obtain room service #30

--- src/services/ReservationService.java
+++ src/services/ReservationService.java
@@ -1,5 +1,8 @@
 package services;
 
+import java.util.Collection;
+import java.util.Map;
+
 import models.IRoom;
 
 public class ReservationService {
@@ -17,6 +20,14 @@ public class ReservationService {
 
     public void addRoom(IRoom room) {
         rooms.put(room.getNumber(), room);
+    }
+
+    public IRoom getARoom(String number) {
+        return rooms.get(number);
+    }
+
+    public Collection<IRoom> getAllRooms() {
+        return rooms.values();
     }
 
     public static getInstance() {
