9d9f78513f5e33d52465fc01407004b0e3eb6840
ioni5 <ioni5.goto@gmail.com>
2023-04-12 11:33:13 -0400

    impl. create customer resource #11

--- src/resources/HotelResource.java
+++ src/resources/HotelResource.java
@@ -1,8 +1,25 @@
 package resources;
 
+import services.CustomerService;
+
 public class HotelResource {
 
+    private static HotelResource instance;
+
+    private CustomerService customerService;
+
+    private HotelResource() {
+        customerService = CustomerService.getInstance();
+    }
+
     public void createACustomer(String firstName, String lastName, String email) {
-        
+        customerService.addCustomer(firstName, lastName, email);
+    }
+
+    public static HotelResource getInstance() {
+        if (instance == null) {
+            instance = new HotelResource();
+        }
+        return instance;
     }
 }
